d3e5ef4299cfeed2ba32c60c8ffee0a2f9f635bd
付一洋 <fuyy0016@gmail.com>
2025-09-29 15:17:34 +0800

    docs: 更新简历项目沉淀描述

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
  */
 // const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
